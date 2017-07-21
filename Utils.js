9c58f37184b4ab6fa968a1d5371b56a00c63e2c2
Kyle Morse <kmorse@rallydev.com>
2017-07-21 08:58:25 -0600

    Added screenshot

---
+++ Utils.js
@@ -0,0 +1,22 @@
+Ext.define('Utils', {
+    singleton: true,
+
+    getFieldForAggregationType: function(aggregationType) {
+        switch(aggregationType) {
+            case 'acceptedleafcount':
+                return 'AcceptedLeafStoryCount';
+            case 'acceptedleafplanest':
+                return 'AcceptedLeafStoryPlanEstimateTotal';
+            case 'leafcount':
+                return 'LeafStoryCount';
+            case 'leafplanest':
+                return 'LeafStoryPlanEstimateTotal';
+            case 'prelimest':
+                return 'PreliminaryEstimateValue';
+            case 'refinedest':
+                return 'RefinedEstimate';
+            default:
+                return null;
+        }
+    }
+});
