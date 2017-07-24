520c44796ce8d48e5bd972e578f93d7780648279
Kyle Morse <kmorse@rallydev.com>
2017-07-24 08:51:52 -0600

    Fix broken calculator tests

--- test/CalculatorSpec.js
+++ test/CalculatorSpec.js
@@ -2,17 +2,13 @@ describe('Calculator', function () {
 
     var data, store;
 
-    function expectChartDataToBe(chartData, categories, cycleTimeSeriesData, percentileSeriesData) {
+    function expectChartDataToBe(chartData, categories, velocitySeriesData) {
         expect(chartData.categories).toEqual(categories); 
-        expect(chartData.series.length).toBe(2);
-        var cycleTimeSeries = chartData.series[0];
-        expect(cycleTimeSeries.name).toBe('Cycle Time');
-        expect(cycleTimeSeries.type).toBe('column');
-        expect(cycleTimeSeries.data).toEqual(cycleTimeSeriesData);
-        var percentileSeries = chartData.series[1];
-        expect(percentileSeries.name).toBe('p25 - p75');
-        expect(percentileSeries.type).toBe('errorbar');
-        expect(percentileSeries.data).toEqual(percentileSeriesData);
+        expect(chartData.series.length).toBe(1);
+        var velocitySeries = chartData.series[0];
+        expect(velocitySeries.name).toBe('Velocity');
+        expect(velocitySeries.type).toBe('column');
+        expect(velocitySeries.data).toEqual(velocitySeriesData);
     }
 
     beforeEach(function () {
@@ -20,11 +16,11 @@ describe('Calculator', function () {
         data = Rally.test.Mock.dataFactory.getRecords('portfolioitem/feature', {
             count: 5,
             values: [
-                { ActualStartDate: '2017-03-02T00:00:00.000Z', ActualEndDate: '2017-03-06T00:00:00.000Z', Release: { _refObjectName: 'Release 1' } },
-                { ActualStartDate: '2017-03-02T00:00:00.000Z', ActualEndDate: '2017-03-03T00:00:00.000Z', Release: { _refObjectName: 'Release 1' } },
-                { ActualStartDate: '2017-06-04T00:00:00.000Z', ActualEndDate: '2017-06-10T00:00:00.000Z', Release: { _refObjectName: 'Release 2' } },
-                { ActualStartDate: '2017-06-05T00:00:00.000Z', ActualEndDate: '2017-06-08T00:00:00.000Z', Release: { _refObjectName: 'Release 2' } },
-                { ActualStartDate: '2017-06-06T00:00:00.000Z', ActualEndDate: '2017-06-13T00:00:00.000Z', Release: { _refObjectName: 'Release 2' } }
+                { ActualStartDate: '2017-03-02T00:00:00.000Z', ActualEndDate: '2017-03-06T00:00:00.000Z', Release: { _refObjectName: 'Release 1' }, RefinedEstimate: 20 },
+                { ActualStartDate: '2017-03-02T00:00:00.000Z', ActualEndDate: '2017-03-03T00:00:00.000Z', Release: { _refObjectName: 'Release 1' }, RefinedEstimate: 30 },
+                { ActualStartDate: '2017-06-04T00:00:00.000Z', ActualEndDate: '2017-06-10T00:00:00.000Z', Release: { _refObjectName: 'Release 2' }, RefinedEstimate: 10 },
+                { ActualStartDate: '2017-06-05T00:00:00.000Z', ActualEndDate: '2017-06-08T00:00:00.000Z', Release: { _refObjectName: 'Release 2' }, RefinedEstimate: 100 },
+                { ActualStartDate: '2017-06-06T00:00:00.000Z', ActualEndDate: '2017-06-13T00:00:00.000Z', Release: { _refObjectName: 'Release 2' }, RefinedEstimate: 50 }
             ]
         });
         store = Ext.create('Rally.data.wsapi.Store', {
@@ -35,25 +31,28 @@ describe('Calculator', function () {
 
     it('should bucket by quarter', function () {
         var calculator = Ext.create('Calculator', {
-            bucketBy: 'quarter'
+            bucketBy: 'quarter',
+            aggregateBy: 'refinedest'
         });
         var chartData = calculator.prepareChartData(store);
-        expectChartDataToBe(chartData, ['2017 Q1', '2017 Q2'], [['2017 Q1', 2.5], ['2017 Q2', 6]], [[1, 4], [3, 7]]);
+        expectChartDataToBe(chartData, ['2017 Q1', '2017 Q2'], [['2017 Q1', 50], ['2017 Q2', 160]]);
     });
 
     it('should bucket by month', function () {
         var calculator = Ext.create('Calculator', {
-            bucketBy: 'month'
+            bucketBy: 'month',
+            aggregateBy: 'refinedest'
         });
         var chartData = calculator.prepareChartData(store);
-        expectChartDataToBe(chartData, ['Mar \'17', 'Jun \'17'], [['Mar \'17', 2.5], ['Jun \'17', 6]], [[1, 4], [3, 7]]);
+        expectChartDataToBe(chartData, ['Mar \'17', 'Jun \'17'], [['Mar \'17', 50], ['Jun \'17', 160]]);
     });
 
     it('should bucket by release', function () {
         var calculator = Ext.create('Calculator', {
-            bucketBy: 'release'
+            bucketBy: 'release',
+            aggregateBy: 'refinedest'
         });
         var chartData = calculator.prepareChartData(store);
-        expectChartDataToBe(chartData, ['Release 1', 'Release 2'], [['Release 1', 2.5], ['Release 2', 6]], [[1, 4], [3, 7]]);
+        expectChartDataToBe(chartData, ['Release 1', 'Release 2'], [['Release 1', 50], ['Release 2', 160]]);
     });
 });
